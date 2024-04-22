2753ae7dcb95a337a002ce31030fc990a5c346e8
Human <humanbakishi@gmail.com>
2024-04-22 14:56:06 +0200

    Initiial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Primitive data types: prepacked/predefined with the language itself
+// Non-primitive: up to the programmer to define
+
+// Primitive: String, Number, Boolean, undefined, null
+
+let name = 'Human';
+let surname = 'Bakishi';
+let bio = 'Always seek opportunities';
+let email = 'humanbakishi@gmail.com';
+let studies = 'Computer Science';
+let age = 23;
+let phoneNo = '+27 63 5299 240';
+
+
+
+console.log(name, surname, bio, email, studies, age);
+
+// Non-primitive: Array, Object, LinkedLists ...
+// Array of students
+let myProjects = [''];
+console.log({ myProjects });
+console.log(myProjects.length );
