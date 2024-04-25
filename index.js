a29245865b28b382a9ae2efb902c32cc9006abeb
Human <humanbakishi@gmail.com>
2024-04-25 21:29:18 +0200

    Add files

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// Primitive data types: prepacked/predefined with the language itself
-// Non-primitive: up to the programmer to define
-
-// Primitive: String, Number, Boolean, undefined, null
 
 let name = 'Human';
 let surname = 'Bakishi';
@@ -11,12 +7,8 @@ let studies = 'Computer Science';
 let age = 23;
 let phoneNo = '+27 63 5299 240';
 
-
-
 console.log(name, surname, bio, email, studies, age);
 
-// Non-primitive: Array, Object, LinkedLists ...
-// Array of students
 let myProjects = [''];
 console.log({ myProjects });
 console.log(myProjects.length );
