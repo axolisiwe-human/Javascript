63301926a2b2a031fafeb335f7072f9f76c5c3b0
Human <humanbakishi@gmail.com>
2024-04-26 11:37:09 +0200

    updating javascript file

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ let phoneNo = '+27 63 5299 240';
 
 console.log(name, surname, bio, email, studies, age);
 
-let myProjects = [''];
+let myProjects = ['Intro to HTML', 'Intro to CSS', 'Intro to Javascript'];
 console.log({ myProjects });
-console.log(myProjects.length );
+console.log("Number of projects: " + myProjects.length );
